e284f7474cc35626f30152575268ce2859fedb94
MahmoudSayed216 <mahmoud_das@outlook.com>
2026-06-20 22:18:55 +0300

    ekjrf

--- dataset.py
+++ dataset.py
@@ -65,7 +65,7 @@ class SegmentationDS(Dataset):
                   f"but n_classes={self.n_classes}. Inspect {cache_path}.")
 
         self.image_transform = transforms.Compose([
-            transforms.Resize((576, 576)),
+            transforms.Resize((256, 256)),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                   std=[0.229, 0.224, 0.225]),
